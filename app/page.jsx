3bc75d8151d82510863b3ca0682057b4cc4b46d4
SHAIKATHAZRA <shaikathazra@gmail.com>
2025-01-05 01:34:06 +0600

    7th complete home

--- app/page.jsx
+++ app/page.jsx
@@ -4,6 +4,7 @@ import{FiDownload} from "react-icons/fi";
 //components
 import Social from "@/components/Social";
 import Photo from "@/components/Photo";
+import Status from "@/components/Status";
 
 const Home =() =>{
   return (
@@ -11,9 +12,9 @@ const Home =() =>{
         <div className="container mx-auto h-full">
           <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
             {/* text */}
-            <div className="text-center xl:text-left">
+            <div className="text-center xl:text-left order-2 xl:order-none">
               <span className="text-xl">Full-Stack Developer</span>
-              <h1 className="h1 mb-6">Hello I,m <br /> <span className="text-accent">Shaikat Hazra</span> </h1>
+              <h1 className="h1 mb-6">Hello I'm <br /> <span className="text-accent">Shaikat Hazra</span> </h1>
 
               <p className="max-w-[500px] mb-9 text-white/80">
                 I’m a Full-Stack Developer working on AI-driven apps, ML models, and research in green computing, bridging innovation with sustainability.
@@ -29,11 +30,12 @@ const Home =() =>{
               </div>
             </div>
             {/* photo */}
-            <div>
+            <div className="order-1 xl:order-none mb-8 xl:mb-0">
               <Photo />
             </div>
           </div>
         </div>
+        <Status/>
       </section>
      );
 };
